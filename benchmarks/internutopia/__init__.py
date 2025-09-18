88d835091823c8c64c9f8c0d5cca5980966e85c8
kew6688 <kewa990809@gmail.com>
2025-09-18 06:25:03 +0000

    add internutopia benchmark

---
+++ benchmarks/internutopia/__init__.py
@@ -0,0 +1,7 @@
+from internnav.env.vln_multi_env import VlnMultiEnv
+from internnav.env.vln_pe_env import VlnPeEnv
+from internnav.evaluator.vln_multi_evaluator import VlnMultiEvaluator
+from internnav.evaluator.vln_pe_evaluator import VlnPeEvaluator
+
+__all__ = ['VlnPeEnv', 'VlnMultiEnv']
+__all__ = ['Evaluator']
