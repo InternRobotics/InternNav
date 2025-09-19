7d72294ae8840c3299b7c542f084df9fcdbe1294
kew6688 <kewa990809@gmail.com>
2025-09-19 10:36:45 +0000

    import agent

--- scripts/eval/eval.py
+++ scripts/eval/eval.py
@@ -1,15 +1,31 @@
+# This file is the main file to run eval with different configs.
+
 import sys
 
 sys.path.append('.')
 
 import argparse
+import glob
+import importlib
 import importlib.util
+import os
 
 from internnav.evaluator import Evaluator
-from internnav_baselines import agents
 from internnav_benchmarks.internutopia.vln_default_config import get_config
 
-# This file is the main file to run eval with different configs.
+
+# import all agents to register them
+def auto_register_agents(agent_dir: str):
+    # Get all Python files in the agents directory
+    agent_modules = glob.glob(os.path.join(agent_dir, '*.py'))
+
+    # Import each module to trigger the registration
+    for module in agent_modules:
+        if not module.endswith('__init__.py'):  # Avoid importing __init__.py itself
+            module_name = os.path.basename(module)[:-3]  # Remove the .py extension
+            importlib.import_module(
+                f'internnav_baselines.agents.{module_name}'
+            )  # Replace 'agents' with your module's package
 
 
 def parse_args():
@@ -36,9 +52,14 @@ def main():
     evaluator_cfg = load_eval_cfg(args.config, attr_name='eval_cfg')
     cfg = get_config(evaluator_cfg)
     print(cfg)
+
+    # Call this function where you initialize your application
+    auto_register_agents('internnav_baselines/agents')
+
+    # TODO: register evaluator here
+
+    print("--- Evaluator start ---")
     evaluator = Evaluator.init(cfg)
-    print(type(evaluator))
-    print(agents.__all__)
     evaluator.eval()
 
 
