590d1837658648c32cd2f081db30e953668ba37b
wangyukai <kewa990809@gmail.com>
2025-10-10 07:44:00 +0000

    fix test

--- challenge/iros_real_world/sdk/test_agent.py
+++ challenge/iros_real_world/sdk/test_agent.py
@@ -7,15 +7,6 @@ from save_obs import load_obs_from_meta
 from internnav.agent.utils.client import AgentClient
 from internnav.configs.evaluator.default_config import get_config
 
-fake_obs_256 = {
-    'rgb': np.zeros((256, 256, 3), dtype=np.uint8),
-    'depth': np.zeros((256, 256), dtype=np.float32),
-    'instruction': 'go to the red car',
-}
-fake_obs_640 = load_obs_from_meta('./captures/rs_meta.json')
-fake_obs_640['instruction'] = 'go to the red car'
-print(fake_obs_640['rgb'].shape, fake_obs_640['depth'].shape)
-
 
 def load_eval_cfg(config_path, attr_name='eval_cfg'):
     spec = importlib.util.spec_from_file_location("eval_config_module", config_path)
@@ -26,18 +17,31 @@ def load_eval_cfg(config_path, attr_name='eval_cfg'):
 
 
 # test if agent behave normally with fake observation
-def test_agent(cfg_path=None, obs=fake_obs_256):
+def test_agent(cfg_path=None, obs=None):
     cfg = load_eval_cfg(cfg_path, attr_name='eval_cfg')
     cfg = get_config(cfg)
 
     agent = AgentClient(cfg.agent)
     for _ in range(10):
-        action = agent.step(obs)[0]
+        action = agent.step(obs)[0]['action']  # modify your agent to match the output format
         print(f"Action taken: {action}")
         assert action in [0, 1, 2, 3]
 
 
 if __name__ == "__main__":
-    test_agent(cfg_path='/root/main_repo/InternNav/scripts/eval/configs/h1_cma_cfg.py', obs=fake_obs_256)
+    # use your own path
+    cfg_path = '/root/main_repo/InternNav/scripts/eval/configs/h1_cma_cfg.py'
+    rs_meta_path = './captures/rs_meta.json'
+
+    fake_obs_256 = {
+        'rgb': np.zeros((256, 256, 3), dtype=np.uint8),
+        'depth': np.zeros((256, 256), dtype=np.float32),
+        'instruction': 'go to the red car',
+    }
+    fake_obs_640 = load_obs_from_meta(rs_meta_path)
+    fake_obs_640['instruction'] = 'go to the red car'
+    print(fake_obs_640['rgb'].shape, fake_obs_640['depth'].shape)
+
+    test_agent(cfg_path=cfg_path, obs=fake_obs_256)
     # test_agent(cfg_path='scripts/eval/configs/h1_internvla_n1_cfg.py', obs=fake_obs_640)
     print("All tests passed!")
