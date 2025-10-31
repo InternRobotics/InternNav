81501f3d9a5504641a3d2346447b7cbc0f18b96c
wangyukai <kewa990809@gmail.com>
2025-10-31 08:19:28 +0000

    updated to latest doc; fix meaningless sys path append in train.py; fix habitat config dataset path

--- scripts/train/train.py
+++ scripts/train/train.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+sys.path.append('./src/diffusion-policy')
 import logging
 import sys
 from datetime import datetime
